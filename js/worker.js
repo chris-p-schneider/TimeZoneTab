db2cf4c2877e1df45f86384cf78a1093a4ed6e5a
Chris Schneider <85539501+chris-p-schneider@users.noreply.github.com>
2025-08-29 18:23:27 -0400

    - added sidepanel, worker, tab perms to manifest - added worker helpers, open sidebar listener

---
+++ js/worker.js
@@ -0,0 +1,25 @@
+function getCurrentWindow() {
+  return chrome.windows.getCurrent();
+}
+
+function getCurrentTab() {
+  const [currentTab] = chrome.tabs.query({
+    active: true, 
+    lastFocusedWindow: true
+  });
+  return currentTab;
+}
+
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: false })
+  .catch((error) => console.error(error));
+
+chrome.runtime.onMessage.addListener((message, sender) => {
+  if (message.action === 'open_side_panel') {
+    chrome.tabs.query({active: true, currentWindow: true}, 
+      ([tab]) => {
+        chrome.sidePanel.open({ tabId: tab.id });
+      }
+    );
+  }
+});
