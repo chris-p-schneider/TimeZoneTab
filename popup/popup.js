9f6eb126817cc2ea11f5e644a1273479a77fb0e2
Chris Schneider <85539501+chris-p-schneider@users.noreply.github.com>
2025-08-29 18:24:19 -0400

    - added button and logic to popup for opening sidebar

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,11 @@
 console.log('ExtensionTemplate: popup');
+
+function openSidebar() {
+    chrome.runtime.sendMessage({action: 'open_side_panel'});
+    window.close();
+}
+
+addEventListener('load', () => {
+    const sidebarButton = document.querySelector('#sidebar-button');
+    sidebarButton.addEventListener('click', openSidebar);
+});
